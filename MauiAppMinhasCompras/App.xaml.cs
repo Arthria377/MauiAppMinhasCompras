d1f96228cb3fd61300fc59f90fa9efeee10eb27f
sique <sique@AKEMI>
2025-02-27 20:58:29 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppMinhasCompras/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace MauiAppMinhasCompras
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            //MainPage = new AppShell();
+            MainPage = new NavigationPage(new Views.ListaProduto());
+        }
+
+    }
+}
